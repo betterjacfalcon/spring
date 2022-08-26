c1f38dcef1c4c3919f748607e39b07f1e2a0e564
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-08-26 16:51:50 -0600

    Inyection de dependecia por atributo

---
+++ dependecy-injection/src/test/java/com/spring/dependy/DependecyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.dependy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependecyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
